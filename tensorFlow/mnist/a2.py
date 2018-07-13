397c14a0890ba836443745a6f570671756be4311
DESKTOP-T9I5NO4\yaoyuan <yuanyao002@alog.com>
2018-07-13 09:40:43 +0800

    tensorflow test

--- tensorFlow/mnist/a2.py
+++ tensorFlow/mnist/a2.py
@@ -1,39 +1,89 @@
-import tensorflow as tf;
-import numpy as np;
+import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 # import matplotlib.pyplot as plt;
- 
+
 # c = tf.truncated_normal(shape=[5,5,1,32], mean=0, stddev=1)
 
 x = tf.placeholder(tf.float32, shape=[None, 784])
-_y = tf.placeholder(tf.float32, shape=[None, 10])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 
 # with tf.Session() as sess:
 # 	print(sess.run(c))
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape=shape, stddev=0.1)
-  return tf.Variable(initial)
+    initial = tf.truncated_normal(shape=shape, stddev=0.1)
+    return tf.Variable(initial)
+
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_poll_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1,2,2,1])
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 # layer1
-W_conv1 = weight_variable([5,5,1,32])
+W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
-h_conv1 = conv2d(x, W_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
+# layer2
+W_conv2 = weight_variable([5, 5, 32, 64])  # patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) +
+                     b_conv2)  # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)
 
+# 现在，图片尺寸减小到7x7，我们加入一个有1024个神经元的全连接层，用于处理整个图片。
+# 我们把池化层输出的张量reshape成一些向量，乘上权重矩阵，加上偏置，然后对其使用ReLU。
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = weight_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
+# Dropout 减少过拟合
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
+# 输出层 -> 最后，我们添加一个softmax层，就像前面的单层softmax regression一样。
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = weight_variable([10])
 
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# 为了进行训练和评估，我们使用与之前简单的单层SoftMax神经网络模型几乎相同的一套代码，
+# 只是我们会用更加复杂的ADAM优化器来做梯度最速下降，在feed_dict中加入额外的参数keep_prob来控制dropout比例。
+# 然后每100次迭代输出一次日志。
+cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+sess = tf.Session()
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+
+sess.run(init)
+
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g" % (i, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g" % sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
